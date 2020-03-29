fb2b19ab811dd304d96558912e157e155acca131
michalsidorow <sztucznymail123@wp.pl>
2020-03-29 21:48:43 +0200

    Fresh project

---
+++ src/main/java/com/sidorow/galleryApi/GalleryApiApplication.java
@@ -0,0 +1,13 @@
+package com.sidorow.galleryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryApiApplication.class, args);
+	}
+
+}
